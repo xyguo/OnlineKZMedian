fc30f02f4a2a4533341dece6b21ecf78f15ab6ce
xyguo_mac <xyguo.ubd@gmail.com>
2020-05-23 23:55:25 -0400

    add comparison with offline methods

--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 
 ## Structure
 
-* The main algorithm is implemented as a class `OnlineKZMed` in `okzm/okzm.py`.
-* The data structure that supports 
+* `OnlineKZMed`: The main algorithm implemented as a class in `okzm/okzm.py`.
+* `Assignmenter`: The data structure that maintains a clustering solution and supports (not yet) fast nearest-neighbor query.
 
 ## Progress
 
 1. [X] Unit tests for all basic functions
-1. [ ] Use heap in the implementation of class `Assignmenter` to support faster query/modification of nearest facility information 
-2. [ ] Implement the `Assignmenter` class via Cython to accelerate.  
+2. [ ] Use heap in the implementation of class `Assignmenter` to support faster query/modification of nearest facility information 
+3. [ ] Implement the `Assignmenter` class via Cython to accelerate.  
+
+The profiling result shows that the major time-consuming part is in `Assignmenter.can_swap` (occupies > 95% total time), which consists of mainly the following parts:
+* `DistanceMatrix.pairwise_dist_argmin_min`: 53%. 
+* `Assignmenter.nearest_facility`: 15%
+* Rest: 27%
 
 ## Requirements
 
--- experiment_neurips20.py
+++ experiment_neurips20.py
@@ -1,8 +1,11 @@
 import numpy as np
-from okzm import OnlineKZMed, k_means_lloyd, k_median_lloyd
-from utils.get_data import get_realworld_data
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
+from time import time
+
+from okzm import OnlineKZMed, kz_median, kz_means
+from okzm.kzmedian import kzmedian_cost_
+from utils import get_realworld_data, compute_cost
 
 data_name = 'covertype'
 print("Read in data set {} and shuffle it ... ".format(data_name))
@@ -11,30 +14,62 @@ n_features = X.shape[1]
 np.random.shuffle(X)
 
 # take the first 100000 clients
-n_clients = 5000
+n_clients = 2000
 print("Take the first {} data points as client set ... ".format(n_clients))
 C = X[:n_clients]
 
 # init facility positions
-n_facilities = 500
-print("Create {} potential facility locations via pre-clustering done by k-means ... ".format(n_facilities))
+n_facilities = 200
+print("\nCreate {} potential facility locations via pre-clustering done by k-means ... ".format(n_facilities))
 
 # F = k_means_lloyd(C, n_clusters=n_facilities)
+t1_start = time()
 km = KMeans(n_clusters=n_facilities, n_init=1, max_iter=100)
 km.fit(C)
 F = km.cluster_centers_
+pre_clustering_time = time() - t1_start
+print("Pre-clustering takes {} secs".format(pre_clustering_time))
 
-print("Pre-compute distance matrix ... ")
+print("\nPre-compute distance matrix ... ")
+t2_start = time()
 dist_mat = pairwise_distances(C, F)
+dist_mat_time = time() - t2_start
+print("Computing distance matrix takes {} secs".format(dist_mat_time))
 
 # Run!
-verbose = True
+t3_start = time()
+verbose = False
 n_clusters = 20
-okzm = OnlineKZMed(n_clusters=n_clusters, n_outliers=100,
+n_outliers = 100
+okzm = OnlineKZMed(n_clusters=n_clusters, n_outliers=n_outliers,
                    gamma=0, ell=1, epsilon=0.1, debugging=verbose)
-print("Start fitting model ... ")
-okzm.fit(C, F, dist_mat, init_p=10)
+print("\nStart fitting the OKZM model ... ")
+okzm.fit(C, F, dist_mat, init_p=500)
+okzm_time = time() - t3_start
+okzm_cost = compute_cost(C, okzm.cluster_centers, cost_func=kzmedian_cost_, remove_outliers=n_outliers)
+print("\nDone. cost={} (removing {} outliers) or {} (removing {} outliers)"
+      .format(okzm.cost, len(okzm.outlier_indices), okzm_cost, n_outliers))
+print("Facility recourse={}, client recourse={}".format(okzm.facility_recourse, okzm.client_recourse))
+print("Fit OKZM takes {} secs".format(okzm_time))
 
-print("Done. cost={}, facility recourse={}, client recourse={}"
-      .format(okzm.cost, okzm.facility_recourse, okzm.client_recourse))
+# Compared with offline k-median-- method
+t4_start = time()
+offline_kzmed_centers = kz_median(C, n_clusters=n_clusters, n_outliers=n_outliers)
+kzmed_time = time() - t4_start
+offline_kzmed_cost = compute_cost(C, offline_kzmed_centers, cost_func=kzmedian_cost_, remove_outliers=n_outliers)
+print("\nOffline k-median-- method gives cost {} (removing {} outliers)".
+      format(offline_kzmed_cost, n_outliers))
+print("online cost / Offline kz-median cost = {} (removing {} outliers)".
+      format(okzm_cost / offline_kzmed_cost, n_outliers))
+print("Fit offline kz-median takes {} secs".format(kzmed_time))
 
+# Compared with offline k-means-- method
+t5_start = time()
+offline_kzmeans_centers = kz_means(C, n_clusters=n_clusters, n_outliers=n_outliers)
+kzmeans_time = time() - t5_start
+offline_kzmeans_cost = compute_cost(C, offline_kzmeans_centers, cost_func=kzmedian_cost_, remove_outliers=n_outliers)
+print("\nOffline k-means-- method gives cost {} (removing {} outliers)".
+      format(offline_kzmeans_cost, n_outliers))
+print("online cost / Offline kz-means cost = {} (removing {} outliers)".
+      format(okzm_cost / offline_kzmeans_cost, n_outliers))
+print("Fit offline kz-means takes {} secs".format(kzmeans_time))
